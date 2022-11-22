4e2a6a68e8aeb8b521e7bedcbdee942ab23c16da
yawkat <jonas.konrad@oracle.com>
2022-11-22 18:10:31 +0100

    Use OCI sdk 3

--- oci/src/main/java/oci/OciMessageService.java
+++ oci/src/main/java/oci/OciMessageService.java
@@ -2,6 +2,7 @@ package oci;
 
 import app.MessageService;
 import app.Person;
+import com.oracle.bmc.http.client.HttpProvider;
 import io.micronaut.context.annotation.Requires;
 import io.micronaut.context.env.Environment;
 import jakarta.inject.Singleton;
@@ -11,6 +12,6 @@ import jakarta.inject.Singleton;
 public class OciMessageService implements MessageService {
     @Override
     public String getMessage(Person person) {
-        return "Hi " + person.name() + "! Welcome to OCI!";
+        return "Hi " + person.name() + "! Welcome to OCI! The http provider is " + HttpProvider.getDefault().toString();
     }
 }
